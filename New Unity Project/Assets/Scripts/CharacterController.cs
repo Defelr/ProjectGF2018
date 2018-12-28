7dc10837af946697cd6cb9d26e6b0bf02a5f8935
Rob <trombonedrummer@gmail.com>
2018-12-28 10:46:27 -0500

    New Branch with Unity Test Project
    - Created a test branch with a Unity project.
    - Added Overworld test scene
    - Imported Armored Player and four grass isometric tiles
    - Added idle and walking animations, linked animations to player
    - Added player controller script to cycle animations

---
+++ New Unity Project/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,31 @@
+﻿/*Basic script to dictate all player actions, currently only changes animations based on inputs */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Animator player_animator; //animator for player object
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_animator = this.GetComponent<Animator>(); //ties player animator to animator variable
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2")) //on right cick
+        {
+            player_animator.SetBool("Walking", true); //transition from idle to walking
+            Debug.Log("Right click");
+        }
+        if (Input.GetButtonDown("Fire1")) //on left cick
+        {
+            player_animator.SetBool("Walking", false); //transition from walking to idle
+            Debug.Log("left click");
+        }
+        
+    }
+}
